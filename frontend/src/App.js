ec45cf25f33e880e5cc6c7fd4efc1d5d6cdc8df5
gabrielANASH <abcdefgabriel99@gmail.com>
2025-07-04 22:39:13 +0530

    local

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
 import Navbar from "./components/Navbar";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
