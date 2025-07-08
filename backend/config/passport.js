3086dc09c333f00653c86d010025a8e9f74a7a00
gabrielANASH <abcdefgabriel99@gmail.com>
2025-07-08 17:13:15 +0530

    LOCAL4

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,7 +7,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.CLIENT_URL}/api/auth/google/callback`,
+        callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
 
       },
       async (accessToken, refreshToken, profile, done) => {
